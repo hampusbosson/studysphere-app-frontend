3b55e0a31ccf99782aacba0c10cb43c38d094c2a
Hampus Bosson <hampus.bosson@outlook.com>
2025-01-11 11:13:53 +0100

    Display confirmation modal when clicking submit button

--- src/Routes/ResetPassword.tsx
+++ src/Routes/ResetPassword.tsx
@@ -1,40 +1,43 @@
-import React from "react";
-import Logo from "../components/Header/Logo";
+import React, { useState } from "react";
+import { api } from "../utils/api";
+import { useNavigate, useSearchParams } from "react-router-dom";
+import { AxiosError } from "axios";
 
 const ResetPassword: React.FC = () => {
+  const [password, setPassword] = useState("");
+  const [message, setMessage] = useState("");
+  const [searchParams] = useSearchParams();
+  const navigate = useNavigate();
 
-    const handleSubmit = () => {
-        console.log('submitted')
+  const token = searchParams.get("token");
+
+  const handleSubmit = async (e: React.FormEvent) => {
+    e.preventDefault();
+
+    try {
+      await api.post("/reset-password", { token, newPassword: password });
+      setMessage("Password reset successful!");
+      navigate("/login");
+    } catch (error: unknown) {
+        if (error instanceof AxiosError && error.response?.data?.message) {
+            setMessage(error.response?.data?.message || "Something went wrong.");
+        }
     }
-    
+  };
 
   return (
-    <div className="w-full flex flex-col justify-center items-center bg-background h-screen">
-      <div className="absolute top-4 left-4 p-2">
-        <Logo clickable={true} size={48} />
-      </div>
-      <div className="flex flex-col justify-center items-center gap-2 mb-6">
-        <h1 className="text-silver text-4xl md:text-5xl font-bold">
-          Reset Password
-        </h1>
-        <h2 className="text-gray-300 font-semibold">Please enter your email adress</h2>
-      </div>
-      <form
-        onSubmit={handleSubmit}
-        className="flex flex-col gap-4 w-full justify-center items-center"
-      >
+    <div>
+      <h1>Reset Your Password</h1>
+      <form onSubmit={handleSubmit}>
         <input
-          type="text"
-          placeholder="Email Address"
-          className="border border-zinc-600 bg-[#1A1C2999] placeholder-gray-300 p-3 rounded-lg w-72 focus:outline-accent"
+          type="password"
+          placeholder="Enter new password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
         />
-        <button
-          type="submit"
-          className="bg-transparent text-white py-2 px-4 rounded-lg border-silver border w-72 hover:bg-accent transition duration-200 hover:border-accent font-semibold mt-2"
-        >
-          Reset Password
-        </button>
+        <button type="submit">Reset Password</button>
       </form>
+      {message && <p>{message}</p>}
     </div>
   );
 };
