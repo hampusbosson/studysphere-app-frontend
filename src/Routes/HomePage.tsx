ce65727c9fa0aa1736cb1704aabb40860598d1ff
Hampus Bosson <hampus.bosson@outlook.com>
2025-01-15 14:28:23 +0100

    Refactor

--- src/Routes/HomePage.tsx
+++ src/Routes/HomePage.tsx
@@ -1,45 +1,48 @@
-import React, { useState, useEffect} from "react";
+import React, { useState, useEffect } from "react";
 import Layout from "../Layout";
 import SideBar from "../components/Sidebar/Sidebar";
 import icons from "../assets/icons/icons";
 import NewClassModal from "../components/Main/NewClassModal";
 import { getClasses, Class } from "../utils/classUtils";
 
-
-
 const HomePage: React.FC = () => {
-  const [modalVisible, setModalVisible] = useState(false);
-  const [classes, setClasses] = useState<Class[]>([])
-  const [activeClass, setActiveClass] = useState('');
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const [classes, setClasses] = useState<Class[]>([]);
+  const [activeClass, setActiveClass] = useState("");
 
+  const openModal = () => setIsModalOpen(true);
+  const closeModal = () => setIsModalOpen(false);
 
-  const openModal = () => setModalVisible(true);
-  const closeModal = () => setModalVisible(false);
+  // Fetch classes from the database on mount
+  useEffect(() => {
+    const fetchClasses = async () => {
+      try {
+        const userClasses = await getClasses();
+        setClasses(userClasses); // Update classes with fetched data
+        setActiveClass(userClasses[0].name);
+      } catch (error) {
+        console.error(error);
+      }
+    };
 
-    // Fetch classes from the database on mount
-    useEffect(() => {
-      const fetchClasses = async () => {
-        try {
-          const userClasses = await getClasses();
-          setClasses(userClasses); // Update classes with fetched data
-          setActiveClass(userClasses[0].name);
-        } catch (error) {
-          console.error(error);
-        }
-      };
-  
-      fetchClasses();
-    }, []);
-  
+    fetchClasses();
+  }, []);
 
   return (
     <Layout>
       <div className="bg-background h-full grid grid-cols-6">
         <div className="col-span-1">
-          <SideBar classes={classes} setActiveClass={setActiveClass} setClasses={setClasses} activeClass={activeClass}/>
+          <SideBar
+            classes={classes}
+            setActiveClass={setActiveClass}
+            setClasses={setClasses}
+            activeClass={activeClass}
+          />
         </div>
         <div className="col-span-5 flex flex-row justify-between items-start p-8">
-          <h1 className="text-4xl font-semibold font-montserrat">{activeClass}</h1>
+          <h1 className="text-4xl font-semibold font-montserrat">
+            {activeClass}
+          </h1>
           <button
             className="flex flex-row items-center gap-2 bg-accent pl-3 pr-4 py-2 rounded-lg hover:bg-accentHover"
             onClick={openModal}
@@ -48,7 +51,9 @@ const HomePage: React.FC = () => {
             <p className="font-semibold text-lg">New Class</p>
           </button>
         </div>
-        {modalVisible && <NewClassModal onClose={closeModal} setClasses={setClasses}/>}
+        {isModalOpen && (
+          <NewClassModal onClose={closeModal} setClasses={setClasses} />
+        )}
       </div>
     </Layout>
   );
