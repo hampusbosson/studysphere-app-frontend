8b5876845030155041a4b84d10d0f3e62282ed99
Hampus Bosson <hampus.bosson@outlook.com>
2025-01-16 10:23:03 +0100

    Add optional label prop

--- src/components/Auth/InputField.tsx
+++ src/components/Auth/InputField.tsx
@@ -3,6 +3,7 @@ import React from "react";
 interface InputFieldProps {
   type: string;
   value: string;
+  label?: string;
   placeholder?: string; // Optional placeholder
   onValueChange: (newValue: string) => void; // Called on input change
   onSubmit?: () => void; // Optional submit handler
@@ -11,6 +12,7 @@ interface InputFieldProps {
 const InputField: React.FC<InputFieldProps> = ({
   type,
   value,
+  label,
   placeholder,
   onValueChange,
   onSubmit,
@@ -23,14 +25,17 @@ const InputField: React.FC<InputFieldProps> = ({
   };
 
   return (
-    <input
-      type={type}
-      value={value}
-      placeholder={placeholder}
-      onChange={(e) => onValueChange(e.target.value)} // Handle value changes
-      onKeyPress={handleKeyPress} // Handle Enter key press
-      className="border border-zinc-600 bg-[#1A1C2999] placeholder-gray-300 p-3 rounded-lg w-72 focus:outline-accent"
-    />
+    <div className="flex flex-col gap-2">
+      {label && <label className="text-gray-300 font-medium">{label}</label>}
+      <input
+        type={type}
+        value={value}
+        placeholder={placeholder}
+        onChange={(e) => onValueChange(e.target.value)} // Handle value changes
+        onKeyPress={handleKeyPress} // Handle Enter key press
+        className="border border-zinc-600 bg-[#1A1C2999] placeholder-gray-300 p-3 rounded-lg w-72 focus:outline-accent"
+      />
+    </div>
   );
 };
 
