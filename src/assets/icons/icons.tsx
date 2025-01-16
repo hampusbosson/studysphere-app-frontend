e2cd5966388b1ae93d4b651bebcba4890c9c110e
Hampus Bosson <hampus.bosson@outlook.com>
2025-01-16 12:29:18 +0100

    Add url and upload icons

--- src/assets/icons/icons.tsx
+++ src/assets/icons/icons.tsx
@@ -411,6 +411,38 @@ const icons = {
       />
     </svg>
   ),
+  linkIcon: (stroke: string) => (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      fill="none"
+      viewBox="0 0 24 24"
+      strokeWidth={1.5}
+      stroke=""
+      className={`size-6 stroke-${stroke}`}
+    >
+      <path
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        d="M13.19 8.688a4.5 4.5 0 0 1 1.242 7.244l-4.5 4.5a4.5 4.5 0 0 1-6.364-6.364l1.757-1.757m13.35-.622 1.757-1.757a4.5 4.5 0 0 0-6.364-6.364l-4.5 4.5a4.5 4.5 0 0 0 1.242 7.244"
+      />
+    </svg>
+  ),
+  cloudIcon: (stroke: string) => (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      fill="none"
+      viewBox="0 0 24 24"
+      strokeWidth={1.5}
+      stroke=""
+      className={`size-6 stroke-${stroke}`}
+    >
+      <path
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        d="M12 16.5V9.75m0 0 3 3m-3-3-3 3M6.75 19.5a4.5 4.5 0 0 1-1.41-8.775 5.25 5.25 0 0 1 10.233-2.33 3 3 0 0 1 3.758 3.848A3.752 3.752 0 0 1 18 19.5H6.75Z"
+      />
+    </svg>
+  ),
 };
 
 export default icons;
