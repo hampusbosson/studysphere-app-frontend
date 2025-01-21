0f15b8937c86e4b56f5eef5849dc96e03663708d
Hampus Bosson <hampus.bosson@outlook.com>
2025-01-21 23:19:41 +0100

    Add navigation buttons at the top of lectureBoc

--- src/Routes/Home/LecturePage.tsx
+++ src/Routes/Home/LecturePage.tsx
@@ -1,15 +1,71 @@
-import React from "react";
-import { useParams } from "react-router-dom";
+import React, { useState } from "react";
+import { /*useParams,*/ useLocation, useNavigate } from "react-router-dom";
+//import { Lecture } from "../../utils/lectureUtils";
 
 const LecturePage: React.FC = () => {
-    const { lectureId } = useParams<{ lectureId: string }>();
+  const navigate = useNavigate();
+  //const { lectureId } = useParams<{ lectureId: string }>();
+  const location = useLocation();
+  const { lecture, classItem, setActiveClass } = location.state || {};
 
-    return (
-        <div>
-            <h1>Lecture Details</h1>
-            <p>Lecture ID: {lectureId}</p>
+  const [activeButton, setActiveButton] = useState("summary");
+
+  const handleClassClick = () => {
+    setActiveClass(classItem)
+    navigate("/home");
+  };
+
+  const handleSummaryClick = () => {
+    setActiveButton("summary");
+  };
+
+  const handleChatClick = () => {
+    setActiveButton("chat");
+  };
+
+  const handleQuizClick = () => {
+    setActiveButton("quiz");
+  };
+
+  return (
+    <div className="px-24">
+      <div className="flex flex-row gap-2 text-gray-400 text-sm">
+        <button onClick={handleClassClick}>{classItem.name}</button>
+        <p> / </p>
+        <p className="text-white">{lecture.title}</p>
+      </div>
+      <div className="bg-gray-900 rounded-lg mt-2 p-2">
+        <div className="flex flex-row justify-around font-bold w-full gap-2">
+          <button
+            className={`py-3 w-full rounded-lg ${
+              activeButton === "summary" ? "bg-gray-800" : "bg-gray-700"
+            }`}
+            onClick={handleSummaryClick}
+          >
+            Summary
+          </button>
+          <button
+            className={`py-3 w-full rounded-lg ${
+              activeButton === "chat" ? "bg-gray-800" : "bg-gray-700"
+            }`}
+            onClick={handleChatClick}
+          >
+            Chat
+          </button>
+          <button
+            className={`py-3 w-full rounded-lg ${
+              activeButton === "quiz" ? "bg-gray-800" : "bg-gray-700"
+            }`}
+            onClick={handleQuizClick}
+          >
+            Quiz
+          </button>
         </div>
-    )
-}
+        <h1>Lecture Details</h1>
+        <p>title: {lecture.title}</p>
+      </div>
+    </div>
+  );
+};
 
 export default LecturePage;
