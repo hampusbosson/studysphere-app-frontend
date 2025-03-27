6ebb6d7aab9bb11513983701f19425562f4ba954
Hampus Bosson <hampus.bosson@outlook.com>
2025-03-27 19:57:10 +0100

    refactor: put pdfViewer component in its own file

--- src/features/course-material/components/course-content.tsx
+++ src/features/course-material/components/course-content.tsx
@@ -1,42 +1,6 @@
 import React from "react";
 import { Tiptap } from "./TextEditor";
-import { Document, Page, pdfjs } from "react-pdf";
-import { useState } from "react";
-
-// Set up PDF.js worker for Vite
-pdfjs.GlobalWorkerOptions.workerSrc = "/pdf.worker.min.mjs";
-
-interface PdfViewerProps {
-  url: string;
-}
-
-const PdfViewer: React.FC<PdfViewerProps> = ({ url }) => {
-  const [numPages, setNumPages] = useState<number | null>(null);
-
-  const onDocumentLoadSuccess = ({ numPages }: { numPages: number }) => {
-    setNumPages(numPages);
-  }
-
-  return (
-    <div className="flex justify-center">
-      <Document
-        file={url}
-        onLoadSuccess={onDocumentLoadSuccess}
-        onLoadError={(error) => console.error("Error loading PDF:", error)}
-        onSourceError={(error) =>
-          console.error("Error with PDF source:", error)
-        }
-      >
-        {numPages &&
-          Array.from(new Array(numPages), (el, index) => (
-            <div key={`page_${index + 1}`} className="m-0 pb-4 space-y-0">
-              <Page pageNumber={index + 1} scale={1.5} renderAnnotationLayer={false} renderTextLayer={false}/>
-            </div>
-          ))}
-      </Document>
-    </div>
-  );
-};
+import PdfViewer from "./pdf-viewer";
 
 interface CourseContentProps {
   activeState: string;
