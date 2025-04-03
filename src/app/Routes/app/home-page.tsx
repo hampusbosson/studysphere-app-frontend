51ca31d09efd4565ac9138642ad52a79859698e3
Hampus Bosson <hampus.bosson@outlook.com>
2025-04-03 18:58:30 +0200

    feat: add active courses tab to dashboard

--- src/app/Routes/app/home-page.tsx
+++ src/app/Routes/app/home-page.tsx
@@ -1,15 +1,19 @@
-import React, { useEffect } from "react";
+import React from "react";
 import DashboardLayout from "../../../components/layouts/dashboard-layout";
+import CourseOverview from "../../../features/courses/components/course-overview";
 
 const HomePage: React.FC = () => {
 
   return (
     <DashboardLayout>
-      <div className="flex flex-col p-8">
+      <div className="flex flex-col py-8 px-12 gap-10 h-full">
         <div>
-          <p className="text-3xl font-semibold">Dashboard</p>
+          <p className="text-4xl font-bold">Dashboard</p>
         </div>
-        <div></div>
+        <div className="flex flex-row gap-10">
+          <CourseOverview />
+          <div>some other content</div>
+        </div>
       </div>
     </DashboardLayout>
   );
